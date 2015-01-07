b3224b486c45918f0b541ef421192b515f4fede8
Andrea Passante <andrea.passante@firmstep.com>
2015-01-07 17:08:41 +0000

    More profile coding

--- handler/user/handler_user_profile.php
+++ handler/user/handler_user_profile.php
@@ -10,11 +10,19 @@ class handler_user_profile extends handler
         need to write the shit for the profile
         */
 
-//        /* get data from the models */
-//        $categories = model_category::getList();
-//
-//		$footerStats = model_statistics::footer();
-//
+        /**
+         * if $GET['nick'] is empty then
+         * wiew the user's profile
+         *
+         * if the user isn't logged in
+         * take to registration form
+         */
+
+        /* get data from the models */
+        $categories = model_category::getList();
+
+		$footerStats = model_statistics::footer();
+
 //		if( $_SESSION['user'] instanceof data_user )
 //		{
 //			/* data for logged in users */
@@ -26,11 +34,19 @@ class handler_user_profile extends handler
 //			$bookmarks = new data_array();
 //		}
 //
-//        $stories = model_story::getLatest();
-//
-//		$popular = model_chapter::getPopular();
-//
-//        $tags = model_statistics::tagsForHomepage( $stories );
+//        $tags = array(
+//            'FAQ'         => 1,
+//            'questions'   => 1,
+//            'stories'     => 1,
+//            'answers'     => 1,
+//            'frequent'    => 1,
+//            'conovelists' => 1,
+//            'asking'      => 1,
+//            'literature'  => 1,
+//            'context'     => 1,
+//            'listed'      => 1,
+//        );
+//        $tags = new data_array( $tags );
 //
 //        /* display data */
 //		$page = new layout_homepage( $categories, $bookmarks, $footerStats, $stories, $popular, $tags );
