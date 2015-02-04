568a2a7043d67c5e0b8445a1850ae2a1ea0033e6
Andrea Passante <andrea.passante@firmstep.com>
2015-02-04 16:43:41 +0000

    13 - added google tracking code

--- layout/layout_page.php
+++ layout/layout_page.php
@@ -52,13 +52,27 @@ abstract class layout_page extends layout
 
     protected function renderBottom()
     {
+
         if( !$_SESSION['user'] instanceof data_user )
         {
             $this->loginForm();
         }
 		
 		echo
+
+        '<script>',
+            "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ ",
+            '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), ',
+            'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) ',
+            "})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ",
+
+            "ga('create', 'UA-15643626-11', 'auto'); ",
+            "ga('send', 'pageview'); ",
+
+        '</script>',
+
         '</body></html>';
+        
     }
 	
 	private function loginForm()
